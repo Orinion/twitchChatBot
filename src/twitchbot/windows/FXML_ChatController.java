134d02a8e5e58230832f009697de9872e6564a4c
Orinion <orinion77@gmail.com>
2016-03-11 18:54:21 +0100

    working tabs

--- src/twitchbot/windows/FXML_ChatController.java
+++ src/twitchbot/windows/FXML_ChatController.java
@@ -8,11 +8,15 @@ package twitchbot.windows;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import twitchbot.ircClient;
 
 /**
@@ -31,16 +35,50 @@ public class FXML_ChatController implements Initializable {
     @FXML
     private Tab newTab;
     @FXML
-    private void handleButtonAction(ActionEvent event)
-    {
+    void handleButtonAction(ActionEvent event) {
         main.myClient.send(textInput.getText());
     }
     
-     @FXML
-    private void handleNewTab(ActionEvent event)
+        @FXML
+    void handleNewTab(Event event) {
+        if(newTab.isSelected())
+            this.addTab(textInput.getText());
+    }
+    private Tab getTab(String tabName)
     {
-        //TODO
-        //Create Select and Name new Tab
+        Tab[] array= (Tab[]) tabPane.getTabs().toArray();
+        for (int i = 0; i < array.length; i++) {
+            if(array[i].getText().equals(tabName))
+                return array[i];
+        }
+        return null;
+    }
+    
+    public Tab addTab(String tab)
+    {
+        if(getTab(tab)!=null)
+            return getTab(tab);
+        Tab addedTab = new Tab(tab);
+        addedTab.setContent(new TextArea());
+        tabPane.getTabs().remove(newTab);
+        tabPane.getTabs().add(addedTab);
+        tabPane.getSelectionModel().select(addedTab);
+        tabPane.getTabs().add(newTab);
+        return addedTab;
+    }
+    public void removeTab(String tab)
+    {
+        Tab act = getTab(tab);
+        if(act!=null)
+            tabPane.getTabs().remove(act); 
+    }
+    public void printMessage(String tab,String message)
+    {
+        Tab act = getTab(tab);
+        if(act == null)
+            act =addTab(tab);
+        TextArea txtArea = (TextArea)act.getContent();
+        txtArea.setText(txtArea.getText()+"\n"+message);
     }
     public void setMain(TwitchBot main)
     {
